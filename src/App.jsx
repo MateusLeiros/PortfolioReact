a382d82404e4573ca679d570716554bce586febf
Mateus Leiros <mateusleiros@gmail.com>
2023-08-03 15:38:11 -0300

    novos arquivos de config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <h1>Meu App</h1>;
+  return (
+    <>
+      <h1>Meu App</h1>
+      <p>Estou comecando meu app a partir de agora</p>
+    </>
+  );
 };
 
 export default App;
